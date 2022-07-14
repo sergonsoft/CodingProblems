4ac18c2a727de31649f3bc97f0e53c4888a8a299
sv <sergonsoft@gmail.com>
2022-07-14 23:43:08 +0200

    Solution for Binary Tree Level Order Traversal

--- Swift/LeetCode/BinaryTreeLevelOrderTraversal.playground/Contents.swift
+++ Swift/LeetCode/BinaryTreeLevelOrderTraversal.playground/Contents.swift
@@ -33,9 +33,47 @@ public class TreeNode {
 
 class Solution {
     func levelOrder(_ root: TreeNode?) -> [[Int]] {
-        let result:[[Int]] = []
+        guard root != nil else {
+            return []
+        }
         
-        return result
+        var level = 0
+        var allNodes:[[TreeNode?]] = []
+        allNodes.append([root])
+        
+        while allNodes.count == level + 1 {
+            let nextLevel = level + 1
+            for node in allNodes[level] {
+                if node?.left != nil {
+                    addNode(&allNodes, nextLevel, node?.left)
+                }
+                
+                if node?.right != nil {
+                    addNode(&allNodes, nextLevel, node?.right)
+                }
+            }
+            
+            level = nextLevel
+        }
+        
+        return treeNodes2Int(allNodes)
+    }
+    
+    fileprivate func addNode( _ nodes:inout [[TreeNode?]], _ level:Int, _ node:TreeNode?) {
+        if nodes.count != level + 1 {
+            nodes.append([node])
+        }
+        else {
+            nodes[level].append(node)
+        }
+    }
+    
+    fileprivate func treeNodes2Int(_ allNodes:[[TreeNode?]]) -> [[Int]] {
+        allNodes.map({nodes in
+            return nodes.map({node in
+                return node!.val
+            })
+        })
     }
 }
 
@@ -50,11 +88,11 @@ let s = Solution()
  Output: [[3],[9,20],[15,7]]
  */
 
-var root = TreeNode(3)
-root.left = TreeNode(9)
-root.right = TreeNode(20)
-root.right?.left = TreeNode(15)
-root.right?.right = TreeNode(7)
+var root:TreeNode? = TreeNode(3)
+root?.left = TreeNode(9)
+root?.right = TreeNode(20)
+root?.right?.left = TreeNode(15)
+root?.right?.right = TreeNode(7)
 var output = [[3],[9,20],[15,7]]
 
 assert(s.levelOrder(root) == output)
@@ -76,7 +114,9 @@ assert(s.levelOrder(root) == output)
  Output: []
  */
 
-root = TreeNode()
-output = [[]]
+root = nil
+output = []
 
 assert(s.levelOrder(root) == output)
+
+print("Solved!!!")
